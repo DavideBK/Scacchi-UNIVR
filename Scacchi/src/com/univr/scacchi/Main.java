06084230fe8865937476a2ade230cf92d06658cd
Davide <Davide@192.168.1.2>
2015-02-07 11:05:31 +0100

    Change test

--- Scacchi/src/com/univr/scacchi/Main.java
+++ Scacchi/src/com/univr/scacchi/Main.java
@@ -3,7 +3,7 @@ package com.univr.scacchi;
 public class Main {
 
 	public static void main(String args[]){
-		System.out.println("Ciao Mondo");
+		System.out.println("Ciao a tutti! Pronti per iniziare");
 	}
 	
 }
